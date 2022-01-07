09e4939d51cb14f0a1045e6d42fd96dc288f5e90
Dimitry Andric <dimitry@unified-streaming.com>
2022-01-07 13:30:34 +0000

    Clarify that this is a Unified Streaming fork of FFmpeg
    Add our own README.md, rename the upstream one to README-upstream.md.
    Add beerware licenses to our build scripts and jamfiles.
    Remove build scripts to build non-free FFmpeg versions.

--- build-msvc-lgpl.sh
+++ build-msvc-lgpl.sh
@@ -1,5 +1,14 @@
 #!/bin/sh -eux
 
+# "THE BEER-WARE LICENSE" (Revision CS-42):
+#
+# This file was written by the CodeShop developers.  As long as you
+# retain this notice you can do whatever you want with it.
+# If we meet some day, and you think this file is worth it, you can
+# buy us a beer in return.  Even if you do that, this file still
+# comes WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
 # Compiler: MSVC 14 (aka Visual Studio 2019)
 # Resulting license: LGPL v2.1
 
